70da9ac17b64609b05c491f6f52c1e604a7da24f
MarcialChoque <marcial.choque@hotmail.com>
2016-03-30 12:30:18 -0400

    update bat script

--- start-all.bat
+++ start-all.bat
@@ -1,12 +1,12 @@
 # start platform
 
 start "config-server"   /D .\config-server       gradle bootRun
-timeout 15
+timeout 20
 start "eureka-registry" /D .\webservice-registry gradle bootRun
-timeout 10
+timeout 20
 start "cargo"           /D .\cargo               gradle bootRun
 start "customer"        /D .\customer            gradle bootRun
 start "financial"       /D .\financial           gradle bootRun
 
-timeout 10
+timeout 25
 start "api-gateway"     /D .\api-gateway         gradle bootRun
