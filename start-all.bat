6fbc3b7898b38a17a53841c68f4030e7d042cb26
MarcialChoque <marcial.choque@hotmail.com>
2016-04-12 18:31:36 -0400

    basic oauth2 integration

--- start-all.bat
+++ start-all.bat
@@ -1,12 +1,13 @@
-# start platform
+REM start platform
 
 start "config-server"   /D .\config-server       gradle bootRun
-timeout 20
+timeout 17
 start "eureka-registry" /D .\webservice-registry gradle bootRun
-timeout 20
+timeout 17
+start "authserver"      /D .\auth-server         gradle bootRun
 start "cargo"           /D .\cargo               gradle bootRun
 start "customer"        /D .\customer            gradle bootRun
 start "financial"       /D .\financial           gradle bootRun
 
-timeout 25
+timeout 35
 start "api-gateway"     /D .\api-gateway         gradle bootRun
